762ce3de68601dec13243e4b1ad636abf503ff23
Peter Omollo <omollpeter@gmail.com>
2024-08-01 18:52:56 +0300

    Write a function that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the following function
+    concat - Returns the concatenation of two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns conactenation of two strings
+    """
+    return str1 + str2
