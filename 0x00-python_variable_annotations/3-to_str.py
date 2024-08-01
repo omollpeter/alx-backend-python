9f1897aee001434b4ea3326b0ff708fa08d347ca
Peter Omollo <omollpeter@gmail.com>
2024-08-01 19:12:26 +0300

    Write a function that returns string representation of a flaot

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the following function
+    to_str - Converts a float to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns a string representation of n
+    """
+    return str(n)
