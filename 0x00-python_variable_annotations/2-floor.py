63b6becdd17d0b171a9d4e3f05bbc168b64eeb0e
Peter Omollo <omollpeter@gmail.com>
2024-08-01 19:05:55 +0300

    Write a function that returns the floor of a decimal number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains the following function
+    floor - Returns the floor of a floating point number
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating point number
+    """
+    return math.floor(n)
