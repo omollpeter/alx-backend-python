3be290e7f78c15bada0d17561deac97d25acfee5
Peter Omollo <omollpeter@gmail.com>
2024-08-02 03:57:11 +0300

    Write a function that returns sum of elements in a list as float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains the following function:
+    sum_list - Returns sum of elements(float) in a list
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Return sum of elements in a list as float
+    """
+    return sum(input_list)
