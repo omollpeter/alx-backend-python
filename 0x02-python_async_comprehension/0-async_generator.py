d8e72941d099fb28e03c65d6b5141ca2358cac9f
Peter Omollo <omollpeter@gmail.com>
2024-08-06 19:01:49 +0300

    Add type annotations

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@ This module contains coroutine definition, async_generator
 
 import asyncio
 import random
+from typing import AsyncGenerator, Any
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None, None]:
     for i in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
