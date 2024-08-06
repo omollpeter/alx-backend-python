b56993813c9ddc939442f94b9adcaca5560a525f
Peter Omollo <omollpeter@gmail.com>
2024-08-06 05:28:01 +0300

    Create a regular function that returns a list of delays from coroutines

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module contains the following function:
+    task_wait_n - Returns a list of delays
+"""
+
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Runs task_wait_random n number of times and returns a list of delays
+    """
+    delays = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    return sorted(delays)
