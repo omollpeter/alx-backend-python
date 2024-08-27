e8e08695db6d3fa8f09c6882f5d168dd5d312e50
Peter Omollo <omollpeter@gmail.com>
2024-08-27 05:30:54 +0300

    Test utils.memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -80,7 +80,7 @@ class TestMemoize(unittest.TestCase):
         class TestClass:
             def a_method(self):
                 return 42
-            
+
             @memoize
             def a_property(self):
                 return self.a_method()
